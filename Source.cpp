999b9a03f43471b8d1e32cfacba0345ec1a2f0d7
kenjinote <kenji256@gmail.com>
2017-03-13 22:04:43 +0900

    initial commit

---
+++ Source.cpp
@@ -0,0 +1,100 @@
+﻿#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#include <windows.h>
+
+TCHAR szClassName[] = TEXT("Window");
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	static HWND hStatic;
+	switch (msg)
+	{
+	case WM_CREATE:
+		hStatic = CreateWindow(TEXT("STATIC"), 0, WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		break;
+	case WM_SIZE:
+		MoveWindow(hStatic, 0, 0, LOWORD(lParam), HIWORD(lParam), TRUE);
+		break;
+	case WM_KEYDOWN:
+		{
+			UINT nScanCode = MapVirtualKey((UINT)wParam, 0);
+			if (nScanCode)
+			{
+				switch (wParam)
+				{
+				case VK_LEFT:
+				case VK_UP:
+				case VK_RIGHT:
+				case VK_DOWN:
+				case VK_PRIOR:
+				case VK_NEXT:
+				case VK_END:
+				case VK_HOME:
+				case VK_INSERT:
+				case VK_DELETE:
+				case VK_DIVIDE:
+				case VK_NUMLOCK:
+					nScanCode |= 0x100;
+					break;
+				}
+				TCHAR szBuf[128];
+				if (GetKeyNameText(nScanCode << 16, szBuf, sizeof(szBuf)))
+				{
+					SetWindowText(hStatic, szBuf);
+				}
+			}
+			else
+			{
+				TCHAR szText[1024];
+				wsprintf(szText, TEXT("Unknown wParam = 0x%X, lParam = 0x%X"), wParam, lParam);
+				SetWindowText(hStatic, szText);
+			}
+		}
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int nCmdShow)
+{
+	MSG msg;
+	WNDCLASS wndclass = {
+		CS_HREDRAW | CS_VREDRAW,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		0,
+		LoadCursor(0,IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW + 1),
+		0,
+		szClassName
+	};
+	RegisterClass(&wndclass);
+	HWND hWnd = CreateWindow(
+		szClassName,
+		TEXT("キーボードから入力されてキーの名前を表示"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		0,
+		CW_USEDEFAULT,
+		0,
+		0,
+		0,
+		hInstance,
+		0
+	);
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
